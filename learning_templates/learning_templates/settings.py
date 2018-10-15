42df49d7cd7888421628182f1cc7a4cc08070d31
慧达到 <huidadao@huidadaoui-MacBookAir.local>
2018-10-15 19:50:39 +0900

    second

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'dow1oh#6j2_lqhol@w$)__1dz(pyi&dk9=h@kyqcw0fvlhj=#l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
